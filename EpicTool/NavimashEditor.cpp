7118ecfd9b6fdf38c525c3b7c98cd6e61b1f856a
SATA1251 <goldpig622@naver.com>
2024-04-17 17:51:56 +0900

    [배성근] 네비메쉬 ui 구현완료

--- EpicTool/NavimashEditor.cpp
+++ EpicTool/NavimashEditor.cpp
@@ -21,10 +21,40 @@ void EpicTool::NavimashEditor::Initialize()
 	_navimashEditor = &KunrealEngine::Navigation::GetInstance();
 }
 
+void EpicTool::NavimashEditor::DrawCylinder(ImDrawList* drawList, ImVec2 windowPos, ImVec2 windowSize, float centerX, float centerY, float radius, float height)
+{
+	ImVec2 center(windowPos.x + centerX, windowPos.y + centerY);
+
+	const int numSegments = 12;
+	const float angleStep = 2 * 3.14 / numSegments;
+
+
+	for (int i = 0; i < numSegments; ++i)
+	{
+		float angle1 = i * angleStep;
+		float angle2 = (i + 1) * angleStep;
+
+		ImVec2 p1(center.x + radius * cosf(angle1), center.y - radius * sinf(angle1));
+		ImVec2 p2(center.x + radius * cosf(angle2), center.y - radius * sinf(angle2));
+
+
+		drawList->AddLine(p1, p2, IM_COL32(0, 255, 0, 255));
+		drawList->AddLine(ImVec2(p1.x, p1.y - height), ImVec2(p2.x, p2.y - height), IM_COL32(0, 255, 0, 255));
+		drawList->AddLine(p1, ImVec2(p1.x, p1.y - height), IM_COL32(0, 255, 0, 255));
+	}
+
+	//drawList->AddCircleFilled(center, radius, IM_COL32(255, 0, 0, 255));
+	drawList->AddCircle(ImVec2(center.x, center.y - ( _agentMaxClimb * 20.0f)), radius, IM_COL32(0, 255, 0, 255));
+	//drawList->AddCircleFilled(ImVec2(center.x, center.y + height), radius, IM_COL32(255, 0, 0, 255));
+}
+
 void EpicTool::NavimashEditor::ShowWindow()
 {
 
 	ImGui::Begin("Navimash");
+
+	ImVec2 windowPos = ImGui::GetWindowPos();
+	ImVec2 windowSize = ImGui::GetWindowSize();
 
 	ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "Navimash");
 
@@ -47,6 +77,10 @@ void EpicTool::NavimashEditor::ShowWindow()
 	ImGui::SameLine();
 
 	ImGui::InputInt("ObjectIndex", &_naviIndex);
+
+	DrawCylinder(ImGui::GetWindowDrawList(), windowPos, windowSize, 70.0f, 350.0f, (_agentRadius * 20.0f), (_agentHeight * 20.0f));
+
+
 
 	ImGui::End();
 
