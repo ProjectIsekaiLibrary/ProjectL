d33d233799172309b05cf2c47062d7a53c6e60fb
PresentKIM <rlaguswo0716@gmail.com>
2024-04-25 20:23:05 +0900

    [김현재] 어거지 큐브맵

--- KunrealEngine/EngineCore.cpp
+++ KunrealEngine/EngineCore.cpp
@@ -95,8 +95,13 @@ void KunrealEngine::EngineCore::Initialize(HWND hwnd, HINSTANCE hInstance, int s
 
 
 	Scene* newWorld = sceneInstance.CreateScene("NewWorld");
+	//// cube map test
+	GRAPHICS->CreateCubeMap("test", "grasscube1024.dds", true);
+	auto list = GRAPHICS->GetCubeMapList();
+	GRAPHICS->SetMainCubeMap(list.back());
+
 	/// 니들 맘대로 해
-	PlayGround();
+	//PlayGround();
 }
 
 void KunrealEngine::EngineCore::Release()
@@ -123,8 +128,8 @@ void KunrealEngine::EngineCore::Update()
 	GraphicsSystem::GetInstance().Update(_editorMousepos.x, _editorMousepos.y);
 	navigationInstance.HandleUpdate(TimeManager::GetInstance().GetDeltaTime());
 
-	std::pair<DirectX::XMFLOAT3, DirectX::XMFLOAT3> pos = kamen->GetComponent<Kamen>()->GetBossPosition();
-	navigationInstance.MoveTempObstacle(pos.first, pos.second);
+	//std::pair<DirectX::XMFLOAT3, DirectX::XMFLOAT3> pos = kamen->GetComponent<Kamen>()->GetBossPosition();
+	//navigationInstance.MoveTempObstacle(pos.first, pos.second);
 	
 	// 장애물 설치 테스트
 	if (inputInstance->KeyInput(KEY::LSHIFT) && inputInstance->MouseButtonDown(0))
@@ -146,8 +151,8 @@ void KunrealEngine::EngineCore::Update()
 
 	inputInstance->GetMousePosition(_ingameMouseX, _ingameMouseY);
 
-	cursorimage->SetPosition(_ingameMouseX, _ingameMouseY);
-	cursorimage->SetScale(0.9 * 0.1, 1.6 * 0.1);
+	//cursorimage->SetPosition(_ingameMouseX, _ingameMouseY);
+	//cursorimage->SetScale(0.9 * 0.1, 1.6 * 0.1);
 
 	auto gameObjectList = GetCurrentScene()->GetObjectList();
 
