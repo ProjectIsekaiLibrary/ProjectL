55f410dfcf2aa448e1b65bd9e784ebf370040558
Spice7 <airforce940706@gmail.com>
2024-05-09 10:19:46 +0900

    [이건호] ㅇㅇ

---
+++ KunrealEngine/SphereCollider.cpp
@@ -0,0 +1,59 @@
+#include "SphereCollider.h"
+#include "GameObject.h"
+#include "Transform.h"
+#include "PhysicsSystem.h"
+
+KunrealEngine::SphereCollider::SphereCollider()
+{
+
+}
+
+KunrealEngine::SphereCollider::~SphereCollider()
+{
+
+}
+
+void KunrealEngine::SphereCollider::Initialize()
+{
+	
+}
+
+void KunrealEngine::SphereCollider::Release()
+{
+	
+}
+
+void KunrealEngine::SphereCollider::FixedUpdate()
+{
+	
+}
+
+void KunrealEngine::SphereCollider::Update()
+{
+	
+}
+
+void KunrealEngine::SphereCollider::LateUpdate()
+{
+	
+}
+
+void KunrealEngine::SphereCollider::OnTriggerEnter()
+{
+	
+}
+
+void KunrealEngine::SphereCollider::OnTriggerStay()
+{
+	throw std::logic_error("The method or operation is not implemented.");
+}
+
+void KunrealEngine::SphereCollider::OnTriggerExit()
+{
+	throw std::logic_error("The method or operation is not implemented.");
+}
+
+void KunrealEngine::SphereCollider::SetActive(bool active)
+{
+	throw std::logic_error("The method or operation is not implemented.");
+}
