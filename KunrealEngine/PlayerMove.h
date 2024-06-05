a393ec9faa2c3ad54980b48ff96e890e7d219199
Spice7 <airforce940706@gmail.com>
2024-06-05 10:40:28 +0900

    [이건호] 프로젝트 설정 / 완벽한 대시 / 스킬 조정중

--- KunrealEngine/PlayerMove.h
+++ KunrealEngine/PlayerMove.h
@@ -35,6 +35,7 @@ namespace KunrealEngine
 		float _errorRange;						// 이 정도 오차범위 이내로 움직이면 움직임이 완료된 것으로 판단
 		int _nodeCount;							// 이동할 노드의 수
 		float _movedRange;						// 대시로 이동한 거리 체크용 변수
+		float _movableRange;					// 대시 가능 거리		// 노드로 계산
 		float _posY;							// 맵마다 다른 높이를 적용할 변수
 
 		// 이동할 위치를 마우스 우클릭 시 업데이트
