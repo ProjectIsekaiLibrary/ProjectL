4fece872cd7ea7401b07361d41090e408ff579c8
SATA1251 <goldpig622@naver.com>
2024-07-11 21:01:15 +0900

    Revert "Merge branch 'BSG'"
    This reverts commit 548028c1ca6734ee3ca440661c7dfaf53e669ac0, reversing
    changes made to 66f396fad7f93f7a5c537dc6c664a2e5f8d3b71b.

--- KunrealEngine/PlayerMove.h
+++ KunrealEngine/PlayerMove.h
@@ -10,8 +10,6 @@ namespace DirectX
 
 namespace KunrealEngine
 {
-	class GameObject;
-
 	class _DECLSPEC PlayerMove : public Component
 	{
 	public:
@@ -60,20 +58,6 @@ namespace KunrealEngine
 		float _tempX;
 		float _tempY;
 		float _tempZ;
-
-		// 대시 파티클
-		std::vector<GameObject*> _playerDashParticleStart;
-		std::vector<GameObject*> _playerDashParticleEnd;
-
-		// 대시 파티클 초기화
-		void DashParticleSetting();
-
-		// 타이머 
-		float _timer;
-
-		// 파티클 출력 처리를 위한 변수
-		bool _isDashStart;
-		bool _isDashEnd;
 
 	public:
 		// 플레이어 이동
